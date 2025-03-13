6221348fdd9281e6ec5a367e6bcba265ac9205ed
WP-devBeep <wagnercp97@gmail.com>
2025-03-13 00:47:03 +0000

    aprendendo operadores

---
+++ atribuição.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero1 = 10, numero2, resultado;
+
+    resultado = 10;
+    printf("resultado: %d\n", resultado);
+    
+    // resultado = resultado + 20
+    resultado += 20;
+    printf("resultado: %d\n", resultado);
+
+    // resultado = resultado - numero1
+    resultado -= numero1;
+    printf("resultado: %d\n", resultado);
+    
+    // resultado = resultado * 5
+    resultado *= 5;
+    printf("resultado: %d\n", resultado);
+    
+    // resultaddo = resultado /2
+    resultado /= 2;
+    printf("resultado: %d\n", resultado);
+    
+
+}
